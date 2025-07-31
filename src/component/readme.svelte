2cc1d65f56b55ebbcab613f1bd660c1c59bb9dea
Kesa <kesaubeeire@gmail.com>
2025-07-31 08:20:39 +0800

    feat: 完善一些<关于>.

--- src/component/readme.svelte
+++ src/component/readme.svelte
@@ -1,17 +1,26 @@
 <script>
   import { version } from '@/../userscript.config.js';
   import { fade } from 'svelte/transition';
-  import { _isFallView } from '@/stores';
+  import { _isFallView, _panelPos } from '@/stores';
   import IconTelegram from '@/assets/icon_telegram.svelte';
 
-  let showModal = false;
+  let _modalFAQ = false;
+  let _modalAbout = false;
 
-  function openModal() {
-    showModal = true;
+  function openFAQ() {
+    _modalFAQ = true;
   }
 
-  function closeModal() {
-    showModal = false;
+  function closeFAQ() {
+    _modalFAQ = false;
+  }
+
+  function openAbout() {
+    _modalAbout = true;
+  }
+
+  function closeAbout() {
+    _modalAbout = false;
   }
 
   let color;
@@ -20,38 +29,67 @@
 
 <div class="entry_mteam">
   <div class="ant-typography" style="line-height: 1.5; text-align: center;">
-    <button class="faq-btn" style:--hover="green">PT-Fall 插件 <span style:color style="font-weight: 600;">[v{version}]</span></button>
-    <button class="faq-btn" style:--hover="#40a9ff" style="cursor: pointer;" on:click={openModal}>常见问题 FAQ</button>
+    <button class="__btn" id="_ptFall_about" style:--hover="green" on:click={openAbout}>PT-Fall<span style:color style="font-weight: 600;">[v{version}]</span></button>
+    <button class="__btn" id="_ptFall_faq" style:--hover="#40a9ff" on:click={openFAQ}>FAQ</button>
   </div>
 </div>
 
+<!-- about 弹窗 -->
+{#if _modalAbout}
+  <div class="modal-overlay" transition:fade={{ duration: 200 }} on:click={closeAbout} role="button" aria-hidden="true">
+    <div class="modal-content" on:click|stopPropagation role="button" aria-hidden="true">
+      <div class="modal-header">
+        <h3>关于 PT-Fall</h3>
+
+        <button class="close-btn" on:click={closeAbout}>&times;</button>
+      </div>
+      <div class="modal-body">
+        <p>PT-Fall 是一个专为 M-team 站点量身定制的瀑布流视图插件</p>
+
+        <!-- github 地址和 greaseFork 地址 -->
+        <p>Github: <a href="https://github.com/KesaubeEire/PT_Fall-View" target="_blank">https://github.com/KesaubeEire/PT_Fall-View</a></p>
+        <p>GreaseFork: <a href="https://greasyfork.org/zh-CN/scripts/543925" target="_blank">https://greasyfork.org/zh-CN/scripts/543925</a></p>
+      </div>
+    </div>
+  </div>
+{/if}
+
 <!-- FAQ 弹窗 -->
-{#if showModal}
-  <div class="modal-overlay" transition:fade={{ duration: 200 }} on:click={closeModal} role="button" aria-hidden="true">
+{#if _modalFAQ}
+  <div class="modal-overlay" transition:fade={{ duration: 200 }} on:click={closeFAQ} role="button" aria-hidden="true">
     <div class="modal-content" on:click|stopPropagation role="button" aria-hidden="true">
       <div class="modal-header">
         <h3>FAQ - 常见问题</h3>
 
-        <button class="close-btn" on:click={closeModal}>&times;</button>
+        <button class="close-btn" on:click={closeFAQ}>&times;</button>
       </div>
       <div class="modal-body">
         <h4>Q: 如何联系反馈问题？</h4>
         <p>
-          <a style:--hover="#40a9ff" class="faq-btn" href="https://t.me/+Nd_qIisDjQ80ZTc9" target="_blank" style="height:40px; display: flex; align-items: center; justify-content: center;"> <IconTelegram height={24} width={24} /> &nbsp;Telegram</a>
+          <a style:--hover="#40a9ff" class="__btn __btnWide" href="https://t.me/+Nd_qIisDjQ80ZTc9" target="_blank"> <IconTelegram height={24} width={24} /> &nbsp;Telegram</a>
         </p>
 
+        <h4>Q: 找不到悬浮框</h4>
+        <button
+          style:--hover="#40a9ff"
+          class="__btn"
+          on:click={() => {
+            $_panelPos = { x: 0, y: 0 };
+          }}>重置悬浮框位置</button
+        >
+
         <h4>Q: 插件没有生效怎么办？</h4>
-        <p>可能是浏览器缓存了请求，尝试使用 Ctrl+Shift+R / Ctrl+F5 强制刷新页面。</p>
+        <p>可能是浏览器缓存了请求<br />尝试使用 Ctrl+Shift+R / Ctrl+F5 强制刷新页面</p>
 
         <h4>Q: 如何切换瀑布流视图？</h4>
-        <p>点击悬浮面板中的"切换显示"按钮即可在瀑布流和原列表视图间切换。</p>
+        <p>点击悬浮面板中的"切换显示"按钮即可在瀑布流和原列表视图间切换</p>
 
         <h4>Q: 如何调整卡片布局？</h4>
-        <p>点击悬浮面板中的"配置菜单"按钮，可以调整卡片的最小 / 最大宽度、间隔等参数。</p>
+        <p>点击悬浮面板中的"配置菜单"按钮<br />可以调整卡片的最小 / 最大宽度、间隔等参数</p>
 
         <h4>Q: 快捷键？</h4>
         <p>x 可以切换瀑布流视图</p>
-        <p>Esc 可以从 次级菜单 / 配置菜单 / iframe 中退出。</p>
+        <p>Esc 可以从 次级菜单 / 配置菜单 / iframe 中退出</p>
       </div>
     </div>
   </div>
@@ -61,13 +99,13 @@
 <svelte:window
   on:keydown|capture={event => {
     if (event.key === 'Escape') {
-      closeModal();
+      closeFAQ();
     }
   }}
 />
 
 <style>
-  .faq-btn {
+  .__btn {
     background-color: var(--bg-2);
     color: white;
     border: none;
@@ -75,9 +113,17 @@
     border-radius: 4px;
     font-size: 14px;
     transition: background-color 0.3s;
+    cursor: pointer;
   }
 
-  .faq-btn:hover {
+  .__btnWide {
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .__btn:hover {
     background-color: var(--hover);
   }
 
